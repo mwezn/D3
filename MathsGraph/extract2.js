26a9487e7f62272af49a50d23657406b64bc81b1
mwezn <m.w.g.nelson@gmail.com>
2023-07-29 13:57:30 +0100

    added up toal profit on betfair

--- MathsGraph/extract2.js
+++ MathsGraph/extract2.js
@@ -7,7 +7,10 @@ const notesCSV = fs.readFileSync(path.resolve(__dirname, "cleanBetData.csv"), "u
 let arr= new Array(notesCSV)
 let allBets=arr[0].split("\n")
 console.log(allBets)
+allBets.splice(0,1)
+console.log(allBets)
 
 let newarr=allBets.map(d=>d.split(",")[11])
 let total=newarr.map(d=>Number(d))
+console.log(newarr,total)
 console.log(total.filter(d=>!isNaN(d)).reduce((a,b)=>a+b,0))
